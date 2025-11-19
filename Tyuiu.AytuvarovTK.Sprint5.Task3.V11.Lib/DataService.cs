eb7135ff70a77bc7bdee2ca740da740c5ac29942
dimka <dimka556.dim4@gmail.com>
2025-11-19 19:43:44 +0500

    Task3

---
+++ Tyuiu.AytuvarovTK.Sprint5.Task3.V11.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AytuvarovTK.Sprint5.Task3.V11.Lib
+{
+    public class DataService : ISprint5Task3V11
+    {
+        public string SaveToFileTextData(int x)
+        {
+            //Дано выражение вычислить его значение при x = 3, результат сохранить в бинарный файл OutPutFileTask3.bin и вывести на консоль. Округлить до трёх знаков после запятой. y = 4 - x^3 / x^2
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            FileInfo FileInfo = new FileInfo(path);
+            bool fileExists = FileInfo.Exists;
+            if (fileExists)
+            {
+                File.Delete(path);
+            }
+            double result = 4 - System.Math.Pow(x, 3) / System.Math.Pow(x, 2);
+            result = System.Math.Round(result, 3);
+            using (BinaryWriter writer = new BinaryWriter(File
+                    .Open(path, FileMode.Create)))
+            {
+                writer.Write(result);
+            }
+            return path;
+        }
+    }
+}

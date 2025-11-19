382111fb721b4ad4f5cdfcd19970f15eb6d3ed6e
dimka <dimka556.dim4@gmail.com>
2025-11-19 19:10:08 +0500

    Task1

---
+++ Tyuiu.AytuvarovTK.Sprint5.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AytuvarovTK.Sprint5.Task1.V4.Lib
+{
+    public class DataService : ISprint5Task1V4
+    {
+        public string SaveToFileTextData(int startValue, int stopValue)
+        {
+            // Дана функция: F(x) = (cos(x) / x + 1) - cos(x) * 1.3 + 3*x Произвести табулирование функции f(x) на заданном диапазоне[-5; 5] с шагом 1. Произвести проверку деления на ноль.При делении на ноль вернуть значение 0. Результат сохранить в текстовый файл OutPutFileTask1.txt и вывести на консоль в таблицу. Значения округлить до двух знаков после запятой.
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask1.txt");
+
+            FileInfo file = new FileInfo(path);
+            bool exists = file.Exists;
+
+            if (exists)
+            {
+                file.Delete();
+            }
+
+            double y;
+            string strY;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    y = 0;
+                }
+                else
+                {
+                    y = (System.Math.Cos(x) / x + 1) - System.Math.Cos(x) * 1.3 + 3 * x;
+                    
+                }
+                y = System.Math.Round(y, 2);
+                strY = y.ToString();
+                File.AppendAllText(path, "F(" + x.ToString() + ") = " + strY + "\n");
+            }
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint5.Task1.V4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvarovTK.Sprint5.Task1.V4.Lib;
+
+namespace Tyuiu.AytuvarovTK.Sprint5.Task1.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            Console.WriteLine("Табулирование функции F(x) на диапазоне [" + startValue + "; " + stopValue + "] с шагом 1:");
+            string path = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Результат сохранён в файл: " + path);
+
+            Console.ReadKey();
+
+        }
+    }
+}

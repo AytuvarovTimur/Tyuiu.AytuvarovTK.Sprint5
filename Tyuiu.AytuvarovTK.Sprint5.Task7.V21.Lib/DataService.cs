1aa01e08ecb15b25c6f5390df3968d1811275aae
dimka <dimka556.dim4@gmail.com>
2025-11-20 17:49:15 +0500

    Task7

---
+++ Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib
+{
+    public class DataService : ISprint5Task7V21
+    {
+        public string LoadDataAndSave(string path)
+        {
+            //Дан файл С:\DataSprint5\InPutDataFileTask7V21.txt (файл взять из архива согласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопировать в неё файл) в котором есть набор символьных данных.Удалить все знаки препинания из файла.Полученный результат сохранить в файл OutPutDataFileTask7V21.txt.
+            string content = File.ReadAllText(path);
+            string pattern = @"[.,!?;:""'\-–—()\[\]{}]";
+            string result = System.Text.RegularExpressions.Regex.Replace(content, pattern, "");
+            string outputPath = Path.Combine(Path.GetDirectoryName(path), "OutPutDataFileTask7V21.txt");
+            File.WriteAllText(outputPath, result);
+            return outputPath;
+
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint5.Task7.V21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib;
+
+namespace Tyuiu.LavrentevVA.Sprint5.Task3.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string inputPath = @"C:\DataSprint5\InputDataFileTask7V21.txt";
+
+            string tempDirectory = Path.GetTempPath();
+            string tempFileName = Path.GetFileName(inputPath);
+            string tempFilePath = Path.Combine(tempDirectory, tempFileName);
+
+            File.Copy(inputPath, tempFilePath, true);
+
+            Console.WriteLine($"Файл скопирован в: {tempFilePath}");
+
+            string result = ds.LoadDataAndSave(tempFilePath);
+
+            string outputPath = Path.Combine(tempDirectory, "OutPutDataFileTask7V29.txt");
+            File.WriteAllText(outputPath, result);
+
+            Console.WriteLine($"Результат сохранён в: {outputPath}");
+            Console.WriteLine("Содержимое результата:");
+            Console.WriteLine(result);
+
+
+            Console.ReadKey();
+        }
+    }
+}

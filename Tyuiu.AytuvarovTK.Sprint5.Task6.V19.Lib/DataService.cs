995871bb8d6d55965a538e82e05e554918d1417c
dimka <dimka556.dim4@gmail.com>
2025-11-20 17:32:38 +0500

    Task6

---
+++ Tyuiu.AytuvarovTK.Sprint5.Task6.V19.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.AytuvarovTK.Sprint5.Task6.V19.Lib
+{
+    public class DataService : ISprint5Task6V19
+    {
+        public int LoadFromDataFile(string path)
+        {
+            // Дан файл С:\DataSprint5\InPutDataFileTask6V19.txt (файл взять из архива согласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопировать в неё файл) в котором есть набор символьных данных.Найти количество двузначных чисел в заданной строке.
+            string content = File.ReadAllText(path);
+            int count = 0;
+            string[] tokens = content.Split(new char[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string token in tokens)
+            {
+                if (int.TryParse(token, out int number))
+                {
+                    if (number >= 10 && number <= 99 || number <= -10 && number >= -99)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.AytuvarovTK.Sprint5.Task6.V19/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using Tyuiu.AytuvarovTK.Sprint5.Task6.V19.Lib;
+
+namespace Tyuiu.LavrentevVA.Sprint5.Task3.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V19.txt";
+            int result = ds.LoadFromDataFile(path);
+            Console.WriteLine("Результат: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}

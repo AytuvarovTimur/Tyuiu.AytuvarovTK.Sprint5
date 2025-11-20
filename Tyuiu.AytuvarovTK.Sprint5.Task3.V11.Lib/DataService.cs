040c6379a32affbf185c216b7156f6196360fb65
dimka <dimka556.dim4@gmail.com>
2025-11-20 16:19:45 +0500

    Tassaassa33

--- Tyuiu.AytuvarovTK.Sprint5.Task3.V11.Lib/DataService.cs
+++ Tyuiu.AytuvarovTK.Sprint5.Task3.V11.Lib/DataService.cs
@@ -13,17 +13,13 @@ namespace Tyuiu.AytuvarovTK.Sprint5.Task3.V11.Lib
             {
                 File.Delete(path);
             }
-            double y = 4.0 - (Math.Pow(x, 3) / Math.Pow(x, 2));
+            double y = (4.0 - (Math.Pow(x, 3)) / Math.Pow(x, 2));
             y = Math.Round(y, 3);
             using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
             {
                 writer.Write(y);
             }
-
-            byte[] fileBytes = File.ReadAllBytes(path);
-            string base64Result = Convert.ToBase64String(fileBytes);
-
-            return base64Result;
+            return path;
         }
     }
 }

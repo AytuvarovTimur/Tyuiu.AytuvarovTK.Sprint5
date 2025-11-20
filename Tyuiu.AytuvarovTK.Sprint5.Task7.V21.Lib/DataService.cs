28ba74a44c3d190ee301ab4e418714a60b26ba71
dimka <dimka556.dim4@gmail.com>
2025-11-20 17:55:51 +0500

    Tsk7

--- Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib/DataService.cs
+++ Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using tyuiu.cources.programming.interfaces.Sprint5;
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
 namespace Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib
 {
     public class DataService : ISprint5Task7V21
@@ -8,10 +9,8 @@ namespace Tyuiu.AytuvarovTK.Sprint5.Task7.V21.Lib
             //Дан файл С:\DataSprint5\InPutDataFileTask7V21.txt (файл взять из архива согласно вашему варианту. Создать папку в ручную С:\DataSprint5\ и скопировать в неё файл) в котором есть набор символьных данных.Удалить все знаки препинания из файла.Полученный результат сохранить в файл OutPutDataFileTask7V21.txt.
             string content = File.ReadAllText(path);
             string pattern = @"[.,!?;:""'\-–—()\[\]{}]";
-            string result = System.Text.RegularExpressions.Regex.Replace(content, pattern, "");
-            string outputPath = Path.Combine(Path.GetDirectoryName(path), "OutPutDataFileTask7V21.txt");
-            File.WriteAllText(outputPath, result);
-            return outputPath;
+            string result = Regex.Replace(content, pattern, string.Empty);
+            return result;
 
         }
     }
